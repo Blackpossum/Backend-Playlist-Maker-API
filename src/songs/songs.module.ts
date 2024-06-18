b3f45518ba1812312b6417d5c45991487dc040d5
Blackpossum <yosaphatharwindra@gmail.com>
2024-06-19 01:33:05 +0700

    fix: songs module class app definition

--- src/songs/songs.module.ts
+++ src/songs/songs.module.ts
@@ -1,12 +1,27 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { SongsController } from './songs.controller';
 import { SongsService } from './songs.service';
+
+const MockingService = {
+  findAll() {
+    return [{ id: 1, tittle: 'one last breath', artist: ['Creed'] }];
+  },
+};
 
 @Module({
   controllers: [SongsController],
   providers: [
     // standard provider technique
-    SongsService,
+    // SongsService,
+    // {
+    //   provide: SongsService, //this is how to use a class Providers methods.
+    //   useClass: SongsService,
+    // },
+    {
+      provide: SongsService, //this is how to use a Value provider Methods.
+      useValue: MockingService,
+    },
   ],
 })
 export class SongsModule {}
